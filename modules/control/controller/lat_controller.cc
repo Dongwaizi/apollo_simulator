62ec30a0013885b189136ae3d1b8aa7e8e13d174
Dongwaizi <128828295+Dongwaizi@users.noreply.github.com>
2023-04-10 20:21:49 +0800

    Update lat_controller.cc

--- modules/control/controller/lat_controller.cc
+++ modules/control/controller/lat_controller.cc
@@ -755,10 +755,10 @@ double LatController::ComputeFeedForward(double ref_curvature) const {
                                   steer_single_direction_max_degree_ * 100;
   } else {
     steer_angle_feedforwardterm =
-        (0910-question+ 0910-question -
-         0910-question*
-             (0910-question -
-              0910-question)) *
+        (wheelbase_*ref_curvature+ kv*v*v*ref_curvature -
+         matrix_k_(0,2)*
+             (lr*ref_curvature -
+             lf_*mass_*v*v*ref_curevature/(2.0*cr_*wheelbase_))) *
         180 / M_PI * steer_ratio_ / steer_single_direction_max_degree_ * 100;
   }
 
