e606f845c21130ccd54c671c0da93eece8518e7e
Dongwaizi <128828295+Dongwaizi@users.noreply.github.com>
2023-04-12 23:05:04 +0800

    Update trajectory_analyzer.cc

--- modules/control/common/trajectory_analyzer.cc
+++ modules/control/common/trajectory_analyzer.cc
@@ -116,12 +116,12 @@ void TrajectoryAnalyzer::ToTrajectoryFrame(const double x, const double y,
 
   // the sin of diff angle between vector (cos_ref_theta, sin_ref_theta) and
   // (dx, dy)
-  double cross_rd_nd = cos_ref_theta*dx-sin_ref_theta*dy;
+  double cross_rd_nd = cos_ref_theta*dy-sin_ref_theta*dx;
   *ptr_d = cross_rd_nd;//轨迹跟踪点坐标下的横向偏差
 
   // the cos of diff angle between vector (cos_ref_theta, sin_ref_theta) and
   // (dx, dy)
-  double dot_rd_nd = cos_ref_theta*dy+sin_ref_theta*dx;
+  double dot_rd_nd = dx * cos_ref_theta + dy * sin_ref_theta;
   *ptr_s = ref_point.s() + dot_rd_nd;//纵向偏差
 
   double delta_theta = theta - ref_point.theta();
